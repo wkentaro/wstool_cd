ac009f7916510c2108d85286a7627440661feaa6
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-27 03:48:14 +0900

    add --managed-only option for wstool_cd command

--- wstool_cd.sh
+++ wstool_cd.sh
@@ -3,10 +3,10 @@
 wstool_cd () {
   local dest
   if [ "$1" = "" ]; then
-    dest=$(wstool info --data-only | sed -n '1p' | awk '{print $2}')
+    dest=$(wstool info --managed-only --data-only | sed -n '1p' | awk '{print $2}')
     cd $dest
   else
-    dest=$(wstool info --only=localname,path | awk 'BEGIN {FS=","} {if ($1 == "'"$1"'") print $2}')
+    dest=$(wstool info --managed-only --only=localname,path | awk 'BEGIN {FS=","} {if ($1 == "'"$1"'") print $2}')
     if [ "$dest" = "" ]; then
       echo "$1 not found"
     else
