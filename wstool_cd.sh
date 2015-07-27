d913f591a4f67290581b7edb37fb14c3c78a5827
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-27 16:38:10 +0900

    Add check lines in wstool_cd

--- wstool_cd.sh
+++ wstool_cd.sh
@@ -2,6 +2,12 @@
 
 wstool_cd () {
   local dest
+  # check if in workspace
+  if ! wstool set >/dev/null 2>&1; then
+    echo 'not in workspace'
+    return 1
+  fi
+  # execute commands
   if [ "$1" = "" ]; then
     dest=$(wstool info --managed-only --data-only | sed -n '1p' | awk '{print $2}')
     cd $dest
