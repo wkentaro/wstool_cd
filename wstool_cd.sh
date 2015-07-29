fe1da259945b9ab98a74f4cc9bfb2bac6a010975
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-29 14:53:57 +0900

    Refactor: add _wstool_cd_get_workspace()

--- wstool_cd.sh
+++ wstool_cd.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 
-wstool_cd () {
-  local dest
-  local ws_root
-  # check if in workspace
-  ws_root=$(python -c "\
+
+_wstool_cd_get_workspace () {
+  python -c "\
 import os
 import wstool.common
 from wstool.cli_common import get_workspace
 try:
-    ws=get_workspace(argv=[], shell_path=os.getcwd(), config_filename='.rosinstall')
+    ws=get_workspace(argv=[], shell_path=os.getcwd(),
+                     config_filename='.rosinstall')
     print(ws)
 except wstool.common.MultiProjectException:
     pass
-")
-  if [ "$ws_root" = "" ]; then
+"
+}
+
+
+wstool_cd () {
+  local dest
+  local ws_root
+  # check if wstool installed
+  which wstool >/dev/null 2>&1 || {
+    echo "please install wstool"
+    return 1
+  }
+  # check if in workspace
+  ws_root=$(_wstool_cd_get_workspace)
+  [ "$ws_root" = "" ] && {
     echo 'not in workspace'
     return 1
-  fi
+  }
   # execute commands
   if [ "$1" = "" ]; then
     cd $ws_root
