3986df5652e65df58681b0adf9984c9388aa53b8
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-29 13:43:08 +0900

    Enhance: specify repo in wstool info for speed

--- wstool_cd.sh
+++ wstool_cd.sh
@@ -13,7 +13,7 @@ wstool_cd () {
     dest=$(wstool info --managed-only --data-only | sed -n '1p' | awk '{print $2}')
     cd $dest
   else
-    dest=$(wstool info --managed-only --only=localname,path | awk 'BEGIN {FS=","} {if ($1 == "'"$1"'") print $2}')
+    dest=$(wstool info $1 2>&1 | grep '^Path' | awk '{print $2}')
     if [ "$dest" = "" ]; then
       echo "$1 not found"
     else
